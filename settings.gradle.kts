994a8875f380670decfb5add772d234a2727ab98
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-01-24 18:02:56 +0100

    Introduce simple Compose Multiplatform App with Kotlin/Wasm target template

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "compose-example"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
