a55d42bf965b3fbfb89c365a459918843eb98e39
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2025-02-11 15:38:03 +0100

    Add experimental FIR checkers

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -80,6 +80,8 @@ kotlin {
                     logger.info("[KUP] ${this@configure.path} : added -Xrender-internal-diagnostic-names")
                     freeCompilerArgs.add("-Wextra")
                     logger.info("[KUP] ${this@configure.path}: added -Wextra")
+                    freeCompilerArgs.add("-Xuse-fir-experimental-checkers")
+                    logger.info("[KUP] ${this@configure.path}: added -Xuse-fir-experimental-checkers")
                 }
             }
         }
